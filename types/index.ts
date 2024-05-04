99cbe8c913b607edd7345f481a5cc65e79635338
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-05-04 04:06:45 -0300

    fix typo

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,10 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: "button" | "submit";
+    title: string,
+    containerStyles?: string,
+    handleClick?: MouseEventHandler<HTMLButtonElement>,
+    btnType?: "button" | "submit",
 }
 
 export interface SearchManufacturerProps {
